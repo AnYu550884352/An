cf7f51949d10c3ff380f374abacd9075fb4f1310
anyu <550884352@qq.com>
2019-08-26 17:39:04 +0800

    改为master

--- app/controller/Index.php
+++ app/controller/Index.php
@@ -7,7 +7,7 @@ class Index extends BaseController
 {
     public function index()
     {
-        return '1111111';
+        return 'master';
     }
 
     public function hello($name = 'ThinkPHP6')
